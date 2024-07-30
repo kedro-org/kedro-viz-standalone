e9553a0577066b4203b2af54098b1c00d8d92b83
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2024-07-30 18:55:23 +0100

    Kedro viz updated to 9.2.0
    Signed-off-by: Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 import "@quantumblack/kedro-viz/lib/styles/styles.min.css";
-import demo from '@quantumblack/kedro-viz/lib/utils/data/demo.mock.json';
-import KedroViz from '@quantumblack/kedro-viz';
+import demo from "@quantumblack/kedro-viz/lib/utils/data/demo.mock.json";
+import KedroViz from "@quantumblack/kedro-viz";
 
 function app() {
   return (
     <div style={{ height: `90vh`, width: `100%` }}>
       <KedroViz
         data={demo}
-        display={{
-          globalToolbar: false,
-          miniMap: false,
-          expandAllPipelines: false,
-          sidebar: false,
+        options={{
+          display: {
+            expandPipelinesBtn: true,
+            exportBtn: true,
+            globalNavigation: true,
+            labelBtn: true,
+            layerBtn: true,
+            metadataPanel: true,
+            miniMap: true,
+            sidebar: true,
+            zoomToolbar: true,
+          },
+          theme: "dark",
         }}
-        visible={{
-          labelBtn: false,
-          layerBtn: false,
-          exportBtn: false,
-          pipelineBtn: false,
-          sidebar: false,
-        }}
-        theme="dark"
       />
     </div>
   );
